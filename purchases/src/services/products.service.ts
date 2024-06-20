f9db55e15e4986e9ed8c6361731bf11b3591ab2a
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-20 20:57:38 -0300

    feat: create product

--- purchases/src/services/products.service.ts
+++ purchases/src/services/products.service.ts
@@ -1,11 +1,38 @@
-import { Injectable } from "@nestjs/common";
+import slugify from 'slugify'
+import { BadRequestException, Injectable } from "@nestjs/common";
+
 import { PrismaService } from "src/database/prisma/prisma.service";
+
+interface createProductParams {
+  title: string
+}
 
 @Injectable()
 export class ProductsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   listAllProducts() {
     return this.prisma.product.findMany()
+  }
+
+  async createProduct({ title }: createProductParams) {
+    const slug = slugify(title, { lower: true })
+
+    const productWithSameSlug = await this.prisma.product.findUnique({
+      where: {
+        slug
+      }
+    })
+
+    if (productWithSameSlug) {
+      throw new BadRequestException('Another product with same slug already exists')
+    }
+
+    return this.prisma.product.create({
+      data: {
+        title,
+        slug
+      }
+    })
   }
 }
