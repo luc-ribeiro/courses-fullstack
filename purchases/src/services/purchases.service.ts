8c46a0fa638e3cc2fe91c01e4221347f4ad1bf30
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-22 14:25:07 -0300

    feat: create kafka service and purchase topic

--- purchases/src/services/purchases.service.ts
+++ purchases/src/services/purchases.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 
 import { PrismaService } from "src/database/prisma/prisma.service";
+import { KafkaService } from "src/messaging/kafka.service";
 
 interface CreatePurchaseParams {
   productId: string
@@ -9,7 +10,10 @@ interface CreatePurchaseParams {
 
 @Injectable()
 export class PurchasesService {
-  constructor(private prisma: PrismaService) { }
+  constructor(
+    private prisma: PrismaService,
+    private kafka: KafkaService
+  ) { }
 
   listAllPurchases() {
     return this.prisma.purchase.findMany({
@@ -41,11 +45,30 @@ export class PurchasesService {
       throw new Error('Product not found')
     }
 
-    return this.prisma.purchase.create({
+    const purchase = this.prisma.purchase.create({
       data: {
         customerId,
         productId
       }
     })
+
+    const customer = await this.prisma.customer.findUnique({
+      where: {
+        id: customerId
+      }
+    })
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId
+      },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug
+      }
+    })
+
+    return purchase
   }
 }
