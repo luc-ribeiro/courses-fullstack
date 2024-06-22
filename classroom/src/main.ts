cee079eed5f05c87c63911f07653594a69c59603
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-22 15:26:34 -0300

    feat: classroom purchase controller kafka

--- classroom/src/main.ts
+++ classroom/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableShutdownHooks()
 
-  await app.listen(3334)
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'classroom',
+        brokers: ['localhost:29092'],
+      }
+    }
+  })
+
+  await app.startAllMicroservices().then(() => {
+    console.log('[Classroom] Microservice started!')
+  })
+
+  await app.listen(3334).then(() => {
+    console.log('[Classroom] HTTP server started!')
+  })
 }
 bootstrap()
---
+++ classroom/src/messaging/controllers/purchases.controller.ts
@@ -0,0 +1,28 @@
+import { Controller } from "@nestjs/common";
+import { EventPattern, Payload } from "@nestjs/microservices";
+
+export interface Customer {
+  authUserId: string
+}
+
+export interface Product {
+  id: string
+  title: string
+  slug: string
+}
+
+interface PurchaseCreatedPayload {
+  customer: Customer
+  product: Product
+}
+
+@Controller()
+export class PurchaseController {
+  
+  @EventPattern('purchases.new-purchase')
+  async purchaseCreate(
+    @Payload() payload: PurchaseCreatedPayload
+  ) {
+    console.log(payload)
+  }
+}
